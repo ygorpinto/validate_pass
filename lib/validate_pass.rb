d9ea2b31a7ac661d62f25f4b8a802e8abf5e2c58
ygorpinto <ygor.pinto@hotmail.com>
2021-09-19 12:25:31 -0300

    validate pass gem

---
+++ lib/validate_pass.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative "validate_pass/version"
+
+module ValidatePass
+  class Error < StandardError; end
+  def initialize(pass)
+    @pass = pass
+  end
+  def validate
+    is_valid = false;
+
+    expression = /(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[*\-!$@#\^%])[A-Za-z\d*\-!$@#\^%]{8,}$/
+
+    is_valid = true if (password =~ expression) == 0
+  end
+end
