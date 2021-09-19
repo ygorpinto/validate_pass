7431c0342fe458f5ed5ca35217a38825cdf19a9d
ygorpinto <ygor.pinto@hotmail.com>
2021-09-19 12:56:27 -0300

    ok

--- lib/validate_pass.rb
+++ lib/validate_pass.rb
@@ -4,16 +4,18 @@ require_relative "validate_pass/version"
 
 module ValidatePass
   class Error < StandardError; end
-  def initialize(pass)
 
+  def initialize(pass)
     validate(pass)
   end
-  def validate(password)
 
+  def validate(password)
     is_valid = false
 
     expression = /(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[*\-!$@#\^%])[A-Za-z\d*\-!$@#\^%]{8,}$/
 
     is_valid = true if (password =~ expression).zero?
+
+    return is_valid
   end
 end
